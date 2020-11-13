54244c8d880f3bb08a6fe596cf2c9130cb65fc70
Савелий <savelussava@mail.ru>
2020-11-13 19:30:42 +0300

    firstversion

---
+++ Fillwords/Button.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Fillwords
+{
+    public class Button
+    {
+        static int width = 20;
+        public int height = 3;
+        public string inputname;
+
+        public Button(string name) 
+        {
+            inputname = name;
+        }
+
+        public void PrintButton (int cursorLeft, int cursorTop)
+        {
+            Console.SetCursorPosition(cursorLeft, cursorTop++);
+            Console.WriteLine($"┌{new string('─', width - 2)}┐");
+            Console.SetCursorPosition(cursorLeft, cursorTop++);
+            Console.WriteLine($"│{new string(' ', (18 - inputname.Length) / 2)}" +
+                              $"{inputname}{new string(' ', (18 - inputname.Length - (18 - inputname.Length) / 2))}│");
+            Console.SetCursorPosition(cursorLeft, cursorTop++);
+            Console.WriteLine($"└{new string('─', width - 2)}┘");
+
+        }
+    }
+}
